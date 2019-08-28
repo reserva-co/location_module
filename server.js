b67af5d0fe9edfc847dc77e39dd526f23e6a0a93
Ingrid <ingridsu@yahoo.com>
2019-08-28 11:51:39 -0700

    Start testing

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const path = require('path')
+const express = require('express');
+
+const app = express();
+const port = 3000;
+const path = require('path');
 const controller = require('./server/controller');
 
-app.use(express.static(path.join(__dirname, 'dist')))
-app.get('/api/location/:id', controller.get)
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('/api/location/:id', controller.get);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
